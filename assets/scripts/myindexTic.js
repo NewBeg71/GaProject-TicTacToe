a6c949234a33fadac89f52bb638fefcf7d40771c
NewBeg71 <dfwg4@hotmail.com>
2017-07-31 21:20:17 -0400

    7/31 GaProject - TicTacToe
    Have psuedo code in js completed except the reset button, as well as,
    sign in and sign up fields.  Need to look at using js to create board so
    that reset button will work.  Look at memory game. ASK FOR HELP.  How
    will api be integrated.  Need to fork from Enterprise to GitHub.

--- assets/scripts/myindexTic.js
+++ assets/scripts/myindexTic.js
@@ -1,12 +1,62 @@
 // Create a Sign-up Button
 
 // Create a Sign-in Button
+// onClick Button brings up an input form to verify the user
 
 // Function to take image and place in grid
 // Make sure can not enter another time
+// Player 1 goes
+let playerOneTurn = true
 
-// Check for winor time
+const playerOneImage = function () {
+  alert('Player 1 Click on your placement choice')
+  if (playerOneTurn === true) {
+    document.getElementById('cell').append('deadLogo')
+  } else {
+    playerOneTurn = false
+    let playerTwoTurn = true
+  }
+  playerOneImage()
+}
+  const playerTwoImage = function () {
+    alert('Player 1 Click on your placement choice')
+    if (playerTwoTurn === true) {
+      document.getElementById('cell').append('buffetLogo')
+    } else {
+    }
+    playerOneTurn = false
+  }
+  playerTwoImage()
+}
+
+// Player 2 goes
+
+// Check for winer time
+const playerOneWins = 0
+const playerOneWins = 0
+
+const winner = function (){
+
 
 // counter to track wins/losses/ties
+// after images are placedin td cells
+const winner = function () {
+// check rows
+  if(#one_one === #one_two === #one_three) {return true}
+  if(#two_one === #two_two === #two_three) {return true}
+  if(#three_one === #three_two === #three_three) {return true}
 
+//check columns
+  if(#one_one === #two_one === #three_one) {return true}
+  if(#one_two === #two_two === #three_two) {return true}
+  if(#one_three === #two_three === #three_three) {return true}
+
+// check diagnolas
+ if(#one_one === #two_#two === #three_three) {return true}
+ if(#one_three === #two_#two) === #three_one) {return true}
+ else{return false}
+
+// return playerOneWins + 1
+// return playerTwoWins + 1
+}
 // Reset button
