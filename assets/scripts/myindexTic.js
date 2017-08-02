bdc36ad0bb95a0bd5f3dad42543070ce6946f2a8
NewBeg71 <dfwg4@hotmail.com>
2017-08-02 08:52:04 -0400

    8/2 TicTacTor - Project 1
    Have nested if/else logic in to function.  Commiting changes to repo.
    Need to test and make sure all brackets are aligned.  Check format of X's
    and O's.  Research how to get images instead of text placed in cells.
    Do wireframe in an app and redo all pre-work.

--- assets/scripts/myindexTic.js
+++ assets/scripts/myindexTic.js
@@ -26,112 +26,247 @@ $(document).ready(function (){
   let turns = 0
   $('table td').on('click', function(){
     // console.log('something') - seems to be working - move on
+    const one_one =$('#one-one')
+    const one_two =$('#one_two')
+    const one_three =$('#one_three')
+    const two_one =$('#two_one')
+    const two_two =$('#two_two')
+    const two_three =$('#two_three')
+    const three_one =$('#three_one')
+    const three_two =$('#three_two')
+    const three_three =$('#three_three')
+
+     // $('td').on('click', function () {
+     // if logic works place above nested in the first function
+    // } replaced this function up above - nest following logic in that function
+
+    // check rows
+
+      if(#one_one.hasClass('0') && #one_two.hasClass('0') && #one_three.hasClass('0')) || // {return true}
+        (#two_one.hasClass('0') && #two_two.hasClass('0') && #two_three.hasClass('0')) || // {return true}
+        (#three_one.hasClass('0') && #three_two.hasClass('0') && #three_three.hasClass('0')) // {return true}
+
+    //check columns
+        (#one_one.hasClass('0') && #two_one.hasClass('0') && #three_one.hasClass('0')) ||// {return true}
+        (#one_two.hasClass('0') === #two_two.hasClass('0') === #three_two.hasClass('0')) || // {return true}
+        (#one_three.hasClass('0') === #two_three.hasClass('0') === #three_three.hasClass('0')) || // {return true}
+
+    // check diagnolas
+        (#one_one.hasClass('0') === #two_#two.hasClass('0') === #three_three.hasClass('0')) || // {return true}
+        (#one_three.hasClass('0') === #two_#two.hasClass('0')) === #three_one.hasClass('0')) {// {return true}
+          alert('Winner is Player 1')
+          // need to not allow another entry ??removeClass?? - look at documentation
+          $('table td').text('+')
+          $('table td').removeClass('disable')
+          $('table td').removeClass('O')
+          $('table td').removeClass('X')
+          }
+      else if
+        (#one_one.hasClass('X') && #one_two.hasClass('X') && #one_three.hasClass('X')) || // {return true}
+        (#two_one.hasClass('X') && #two_two.hasClass('X') && #two_three.hasClass('X')) || // {return true}
+        (#three_one.hasClass('X') && #three_two.hasClass('X') && #three_three.hasClass('X')) // {return true}
+        (#one_one.hasClass('X') && #two_one.hasClass('X') && #three_one.hasClass('X')) ||// {return true}
+        (#one_two.hasClass('X') === #two_two.hasClass('X') === #three_two.hasClass('X')) || // {return true}
+        (#one_three.hasClass('X') === #two_three.hasClass('X') === #three_three.hasClass('X')) || // {return true}
+        (#one_one.hasClass('X') === #two_#two.hasClass('X') === #three_three.hasClass('X')) || // {return true}
+        (#one_three.hasClass('X') === #two_#two.hasClass('X')) === #three_one.hasClass('X')) {// {return true}
+          alert('Winner is Player 2')
+        // need to not allow another entry ??removeClass?? - look at documentation
+          $('table td').text('+')
+          $('table td').removeClass('disable')
+          $('table td').removeClass('O')
+          $('table td').removeClass('X')
+          }
+     else if
+        (turns === 9) { // {return false}
+          alert('Cat\'s Game: A Tie!')
+          $('table td').text('+')
+          $('table td').removeClass('disable')
+          $('table td').removeClass('O')
+          $('table td').removeClass('X')
+    }
+    else if
+      ($(this).hasClass('disable')){
+        alert('This cell has already been selected!')
+      }
+      else if
+        (turns % 2 == 0)
+          turns ++
+          $(this).text('O')
+          $(this)addClass('disable O')
+          (#one_one.hasClass('O') && #one_two.hasClass('O') && #one_three.hasClass('O')) || // {return true}
+          (#two_one.hasClass('O') && #two_two.hasClass('O') && #two_three.hasClass('O')) || // {return true}
+          (#three_one.hasClass('O') && #three_two.hasClass('O') && #three_three.hasClass('O')) // {return true}
+          (#one_one.hasClass('O') && #two_one.hasClass('O') && #three_one.hasClass('O')) ||// {return true}
+          (#one_two.hasClass('O') === #two_two.hasClass('O') === #three_two.hasClass('O')) || // {return true}
+          (#one_three.hasClass('O') === #two_three.hasClass('O') === #three_three.hasClass('O')) || // {return true}
+          (#one_one.hasClass('O') === #two_#two.hasClass('O') === #three_three.hasClass('O')) || // {return true}
+          (#one_three.hasClass('O') === #two_#two.hasClass('O')) === #three_one.hasClass('O')) {
+            alert('Winner is Player 1')
+            turns = 0
+          }
+        else {
+          turns ++
+          $(this).addClass('disable')
+          (#one_one.hasClass('X') && #one_two.hasClass('X') && #one_three.hasClass('X')) || // {return true}
+          (#two_one.hasClass('X') && #two_two.hasClass('X') && #two_three.hasClass('X')) || // {return true}
+          (#three_one.hasClass('X') && #three_two.hasClass('X') && #three_three.hasClass('X')) // {return true}
+          (#one_one.hasClass('X') && #two_one.hasClass('X') && #three_one.hasClass('X')) ||// {return true}
+          (#one_two.hasClass('X') === #two_two.hasClass('X') === #three_two.hasClass('X')) || // {return true}
+          (#one_three.hasClass('X') === #two_three.hasClass('X') === #three_three.hasClass('X')) || // {return true}
+          (#one_one.hasClass('X') === #two_#two.hasClass('X') === #three_three.hasClass('X')) || // {return true}
+          (#one_three.hasClass('X') === #two_#two.hasClass('X')) === #three_one.hasClass('X')) {
+            alert('Winner is Player 2')
+          }
+          $('reset').on('click', function () {
+            $('table td').text('+')
+            $('table td').removeClass('disable')
+            $('table td').removeClass('O')
+            $('table td').removeClass('X')
+            let turns = 0
+          })
   })
 })
 
 // Function to take image and place in grid
 // Make sure can not enter another time
 // Player 1 goes
-let playerOneTurn = true
+//let playerOneTurn = true
 
-const playerOneImage = function () {
-  if (playerOneTurn === true) {
-    alert('Player 1 Click on your placement choice')
-    $( ".cell" ).on('click', function() {
-      $('.cell').onCLick.append("<img src = './Images/deadlogo.jpg' />")
-    } )
-    } else {
-      playerOneTurn = false
-      let playerTwoTurn = true
-    }
-  }
+//const playerOneImage = function () {
+  //if (playerOneTurn === true) {
+    //alert('Player 1 Click on your placement choice')
+    //$( ".cell" ).on('click', function() {
+      //$('.cell').onCLick.append("<img src = './Images/deadlogo.jpg' />")
+    //} )
+    //} else {
+      //playerOneTurn = false
+      //let playerTwoTurn = true
+    //}
+  //}
 
-console.log('something')
+//console.log('something')
 // playerOneImage()
 
 
 
-  const playerTwoImage = function () {
-    alert('Player 1 Click on your placement choice')
-    if (playerTwoTurn === true) {
-    $('.cell').onCLick.append("#buffetLogo")
+  //const playerTwoImage = function () {
+    //alert('Player 1 Click on your placement choice')
+    //if (playerTwoTurn === true) {
+    //$('.cell').onCLick.append("#buffetLogo")
     /* document.getElementById('cell').append('buffetLogo') */
-    } else {
-    }
-    playerTwoTurn = false
-    let playerOneTurn = true
-  }
-  playerTwoImage()
+  //  } else {
+  //  }
+    //playerTwoTurn = false
+    //let playerOneTurn = true
+//  }
+  // playerTwoImage()
 
 
 // Player 2 goes
 
 // Check for winer times
-const playerOneWins = 0
-const playerTwoWins = 0
+// const playerOneWins = 0
+//const playerTwoWins = 0
 
-const winner = function (){
+//const winner = function (){
 
 }
 // counter to track wins/losses/ties
 // after images are placedin td cells
-const one_one =$('#one-one')
-const one_two =$('#one_two')
-const one_three =$('#one_three')
-const two_one =$('#two_one')
-const two_two =$('#two_two')
-const two_three =$('#two_three')
-const three_one =$('#three_one')
-const three_two =$('#three_two')
-const three_three =$('#three_three')
+//const one_one =$('#one-one')
+//const one_two =$('#one_two')
+//const one_three =$('#one_three')
+//const two_one =$('#two_one')
+//const two_two =$('#two_two')
+//const two_three =$('#two_three')
+//const three_one =$('#three_one')
+//const three_two =$('#three_two')
+//const three_three =$('#three_three')
 
-$('td').on('click', function () {
+ // $('td').on('click', function () {
  // if logic works place above nested in the first function
-}
+// } replaced this function up above - nest following logic in that function
+
 // check rows
-  if(#one_one.hasClass('0') && #one_two.hasClass('0') && #one_three.hasClass('0')) || // {return true}
-    (#two_one.hasClass('0') && #two_two.hasClass('0') && #two_three.hasClass('0')) || // {return true}
-    (#three_one.hasClass('0') && #three_two.hasClass('0') && #three_three.hasClass('0')) // {return true}
+
+  //if(#one_one.hasClass('0') && #one_two.hasClass('0') && #one_three.hasClass('0')) || // {return true}
+    //(#two_one.hasClass('0') && #two_two.hasClass('0') && #two_three.hasClass('0')) || // {return true}
+    //(#three_one.hasClass('0') && #three_two.hasClass('0') && #three_three.hasClass('0')) // {return true}
 
 //check columns
-    (#one_one.hasClass('0') && #two_one.hasClass('0') && #three_one.hasClass('0')) ||// {return true}
-    (#one_two.hasClass('0') === #two_two.hasClass('0') === #three_two.hasClass('0')) || // {return true}
-    (#one_three.hasClass('0') === #two_three.hasClass('0') === #three_three.hasClass('0')) || // {return true}
+    //(#one_one.hasClass('0') && #two_one.hasClass('0') && #three_one.hasClass('0')) ||// {return true}
+    //(#one_two.hasClass('0') === #two_two.hasClass('0') === #three_two.hasClass('0')) || // {return true}
+    //(#one_three.hasClass('0') === #two_three.hasClass('0') === #three_three.hasClass('0')) || // {return true}
 
 // check diagnolas
-    (#one_one.hasClass('0') === #two_#two.hasClass('0') === #three_three.hasClass('0')) || // {return true}
-    (#one_three.hasClass('0') === #two_#two.hasClass('0')) === #three_one.hasClass('0')) {// {return true}
-      alert('Winner is Player 1')
+    //(#one_one.hasClass('0') === #two_#two.hasClass('0') === #three_three.hasClass('0')) || // {return true}
+    //(#one_three.hasClass('0') === #two_#two.hasClass('0')) === #three_one.hasClass('0')) {// {return true}
+      //alert('Winner is Player 1')
       // need to not allow another entry ??removeClass?? - look at documentation
-      $('table td').text('+')
-      $('table td').removeClass('disable')
-      $('table td').removeClass('O')
-      $('table td').removeClass('X')
-      }
-  else if
-    (#one_one.hasClass('X') && #one_two.hasClass('X') && #one_three.hasClass('X')) || // {return true}
-    (#two_one.hasClass('X') && #two_two.hasClass('X') && #two_three.hasClass('X')) || // {return true}
-    (#three_one.hasClass('X') && #three_two.hasClass('X') && #three_three.hasClass('X')) // {return true}
-    (#one_one.hasClass('X') && #two_one.hasClass('X') && #three_one.hasClass('X')) ||// {return true}
-    (#one_two.hasClass('X') === #two_two.hasClass('X') === #three_two.hasClass('X')) || // {return true}
-    (#one_three.hasClass('X') === #two_three.hasClass('X') === #three_three.hasClass('X')) || // {return true}
-    (#one_one.hasClass('X') === #two_#two.hasClass('X') === #three_three.hasClass('X')) || // {return true}
-    (#one_three.hasClass('X') === #two_#two.hasClass('X')) === #three_one.hasClass('X')) {// {return true}
-      alert('Winner is Player 2'){
+      //$('table td').text('+')
+      //$('table td').removeClass('disable')
+      //$('table td').removeClass('O')
+      //$('table td').removeClass('X')
+      //}
+  //else if
+    //(#one_one.hasClass('X') && #one_two.hasClass('X') && #one_three.hasClass('X')) || // {return true}
+    //(#two_one.hasClass('X') && #two_two.hasClass('X') && #two_three.hasClass('X')) || // {return true}
+    //(#three_one.hasClass('X') && #three_two.hasClass('X') && #three_three.hasClass('X')) // {return true}
+    //(#one_one.hasClass('X') && #two_one.hasClass('X') && #three_one.hasClass('X')) ||// {return true}
+    //(#one_two.hasClass('X') === #two_two.hasClass('X') === #three_two.hasClass('X')) || // {return true}
+    //(#one_three.hasClass('X') === #two_three.hasClass('X') === #three_three.hasClass('X')) || // {return true}
+    //(#one_one.hasClass('X') === #two_#two.hasClass('X') === #three_three.hasClass('X')) || // {return true}
+    //(#one_three.hasClass('X') === #two_#two.hasClass('X')) === #three_one.hasClass('X')) {// {return true}
+      //alert('Winner is Player 2')
     // need to not allow another entry ??removeClass?? - look at documentation
-    $('table td').text('+')
-    $('table td').removeClass('disable')
-    $('table td').removeClass('O')
-    $('table td').removeClass('X')
- }
- else if
-    (turns === 9) { // {return false}
-      alert('Cat\'s Game: A Tie!')
-      $('table td').text('+')
-      $('table td').removeClass('disable')
-      $('table td').removeClass('O')
-      $('table td').removeClass('X')
-}
+      //$('table td').text('+')
+      //$('table td').removeClass('disable')
+      //$('table td').removeClass('O')
+      //$('table td').removeClass('X')
+      //}
+ //else if
+  //  (turns === 9) { // {return false}
+    //  alert('Cat\'s Game: A Tie!')
+    //  $('table td').text('+')
+    //  $('table td').removeClass('disable')
+    //  $('table td').removeClass('O')
+    //  $('table td').removeClass('X')
+//}
+//else if
+  //($(this).hasClass('disable')){
+    //alert('This cell has already been selected!')
+  //}
+  //else if
+    //(turns % 2 == 0)
+      //turns ++
+      //$(this).text('O')
+      //$(this)addClass('disable O')
+      //(#one_one.hasClass('X') && #one_two.hasClass('X') && #one_three.hasClass('X')) || // {return true}
+      //(#two_one.hasClass('X') && #two_two.hasClass('X') && #two_three.hasClass('X')) || // {return true}
+      //(#three_one.hasClass('X') && #three_two.hasClass('X') && #three_three.hasClass('X')) // {return true}
+      //(#one_one.hasClass('X') && #two_one.hasClass('X') && #three_one.hasClass('X')) ||// {return true}
+      //(#one_two.hasClass('X') === #two_two.hasClass('X') === #three_two.hasClass('X')) || // {return true}
+      //(#one_three.hasClass('X') === #two_three.hasClass('X') === #three_three.hasClass('X')) || // {return true}
+      //(#one_one.hasClass('X') === #two_#two.hasClass('X') === #three_three.hasClass('X')) || // {return true}
+      //(#one_three.hasClass('X') === #two_#two.hasClass('X')) === #three_one.hasClass('X')) {
+        //alert('Winner is Player 1')
+        //turns = 0
+    //  }
+      //else {
+        //turns ++
+        //$(this).addClass('disable')
+        //(#one_one.hasClass('X') && #one_two.hasClass('X') && #one_three.hasClass('X')) || // {return true}
+        //(#two_one.hasClass('X') && #two_two.hasClass('X') && #two_three.hasClass('X')) || // {return true}
+        //(#three_one.hasClass('X') && #three_two.hasClass('X') && #three_three.hasClass('X')) // {return true}
+        //(#one_one.hasClass('X') && #two_one.hasClass('X') && #three_one.hasClass('X')) ||// {return true}
+        //(#one_two.hasClass('X') === #two_two.hasClass('X') === #three_two.hasClass('X')) || // {return true}
+        //(#one_three.hasClass('X') === #two_three.hasClass('X') === #three_three.hasClass('X')) || // {return true}
+        //(#one_one.hasClass('X') === #two_#two.hasClass('X') === #three_three.hasClass('X')) || // {return true}
+        //(#one_three.hasClass('X') === #two_#two.hasClass('X')) === #three_one.hasClass('X')) {
+          //alert('Winner is Player 2')
+      //  }
+
 // return playerOneWins + 1
 // return playerTwoWins + 1
 
